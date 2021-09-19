9b18a90dc9d20212ced33029643be9114c2ed325
pedropamn <19939247+pedropamn@users.noreply.github.com>
2021-09-19 17:00:58 -0300

    Add files via upload

---
+++ simplecrypt.py
@@ -0,0 +1,61 @@
+﻿#Crypt Function
+def crypt(password, fullfilepath):
+    import pyAesCrypt
+
+    #get file extension
+    #filename = fullfilepath.splitext(f)[0]
+    #extension = fullfilepath.splitext(f)[1]
+
+
+    # encryption/decryption buffer size - 64K
+    bufferSize = 64 * 1024
+
+    # encrypt
+    try:
+        pyAesCrypt.encryptFile(fullfilepath, fullfilepath + ".aes", password, bufferSize)
+        done = True
+        
+        #Delete original file
+        import os
+        os.remove(fullfilepath)
+        
+        #Rename .aes file to original extension
+        #os.rename(fullfilepath + ".aes", 'encryptedssss')
+        
+    except Exception as e:
+        done = False
+
+    
+    return done
+
+
+
+#Decrypt Function
+def decrypt(password, fullfilepath):
+    import pyAesCrypt
+
+    # encryption/decryption buffer size - 64K
+    bufferSize = 64 * 1024
+
+    # encrypt
+    try:
+        pyAesCrypt.decryptFile(fullfilepath, fullfilepath + '.temp', password, bufferSize)
+        done = True
+
+        #Delete encrypted file
+        import os
+        os.remove(fullfilepath)
+
+        #Rename .temp file to original extension
+        import os.path
+        original_path_and_name = os.path.splitext(fullfilepath)[0] #/path/to/file.ext
+        os.rename(fullfilepath + '.temp', original_path_and_name)
+        
+        
+    except Exception as e:
+        done = False
+        
+    return done
+    
+
+
